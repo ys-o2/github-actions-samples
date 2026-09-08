28eac9a5db38e74f24f5f2e4ba02cf0f99cfe9e6
ys-o <y-onizuka@brexa.com>
2026-09-08 13:53:45 +0900

    ワークフローのACTIONS部品化、リファクタリング

--- .github/actions/validate-semver/validate-semver.sh
+++ .github/actions/validate-semver/validate-semver.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
 
+VERSION=$1
+SEMVER_REGEX="^v([0-9]+)\.([0-9]+)\.([0-9]+)$"
+if [[ ! "${VERSION}" =~ ${SEMVER_REGEX} ]]; then
+    echo "Invalid version: ${VERSION}"
+    exit 1
+fi
+echo "Version is valid: ${VERSION}"
